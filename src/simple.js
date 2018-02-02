6d2af86527fc7244fc1d1d3f84c893f6dcd869e6
ZhangBowei <919270881@qq.com>
2018-02-02 14:14:04 +0800

    Fri Feb 02 2018 14:14:04 GMT+0800 (CST)

--- src/simple.js
+++ src/simple.js
@@ -1,7 +1,14 @@
 function f2() {
 	var f = function () { return 1; };
     function f() { return '2'; }
+    if (true) {
+        function test() {
+            console.log('tset')
+        }
+    }
+    test()
     return f();
 } // 1
 
 var a = f2();
+console.log(a);
