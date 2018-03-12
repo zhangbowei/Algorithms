777d5a8a808d362c4acdf442eb2eb515a5da8659
ZhangBowei <919270881@qq.com>
2018-03-12 17:00:25 +0800

    Mon Mar 12 2018 17:00:25 GMT+0800 (CST)

--- src/simple.js
+++ src/simple.js
@@ -1,11 +1,89 @@
-function C(x) {
-    this.x = x;
-}
-var a = new C(1);
+/* @id PQLib */
+var PriorityQueue =  (function  () {
+    var counter = 0;
 
+    /* @id Node */
 
-C.prototype.y = 1;
-var b = new C(2);
-console.log(b.x, b.y);
-console.log(a.__proto__);
-console.log(a.x, a.y);
+    var Node = function  (pri, val) {
+
+    this.Pri = pri;
+
+    this.Val = val;
+
+    this.Next = null;
+
+    counter++;
+
+    }
+
+    /* @id insert */
+
+    Node.Prototype.Insert = function  (nl) {
+
+    if  (nl === null) {
+
+    return this
+
+    }
+
+    if  (this.Pri>= nl.Pri) {
+
+    this.Next = nl;
+
+    return this
+
+    }
+
+    var tmp = this.Insert(nl. Next);
+
+    nl.Next = tmp;
+
+    return nl
+
+    }
+
+    /* @id PQ */
+
+    var PQ = function() {
+
+    this._head = null
+
+    };
+
+    /* @id enqueue */
+
+PQ.prototype.enqueue = function (pri, val) {
+
+    if  (counter> 42)
+
+    throw new Error ()
+
+    var n = new Node (pri, val);
+
+    this._head = n.Insert(this._head)
+
+    };
+
+    /* @id dequeue */
+
+    PQ.prototype.Dequeue = function  ()
+
+    if (this._head === null) {
+
+    throw new Error  ()
+
+    }
+
+    var first = this. _head;
+
+    this._head = this._head.Next;
+
+    C 0 unt e r--;
+
+    return {pri: first. Pri,
+
+    val: first. Val}; };
+
+    return PQ; })  ();
+
+    var q = new PriorityQueue (); q. Enqueue (1, "last"); q. Enqueue  (3, "bar"); q. Enqueue  (2, "foo"); var r = q. Dequeue ();
