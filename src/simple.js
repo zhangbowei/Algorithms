2cc2edad43765916a08b4ea403c52162b735a006
ZhangBowei <919270881@qq.com>
2018-04-02 22:06:59 +0800

    Mon Apr 02 2018 22:06:59 GMT+0800 (CST)

--- src/simple.js
+++ src/simple.js
@@ -1,47 +1,26 @@
-var _ = require('lodash');
-(function test() {
-    var s = [
-        [0, 1, 0, 0, 1, 0],
-        [1, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [1, 1, 0, 1, 0, 1],
-        [0, 0, 0, 0, 1, 0],
-    ];
-    function c(dd, num) {
-        var d = _.cloneDeep(dd);
-        if (num === 0)
-            return dd;
-        for (var i = 0; i < d.length; i++)
-            for (var j = 0; j < d[0].length; j++) {
-                dd[i][j] = 0;
-                for (var k = 0; k < d[0].length; k++)
-                    dd[i][j] += d[i][k] * s[k][j];
-            }
-        return c(dd, --num);
+function SuperClass(name) {
+    this.name = name;
+    this.books = ['JS', 'html', 'css'];
+}
+SuperClass.prototype.getName = function() {
+    console.log(this.name);
+}
+var inheritPrototype = function(parent, child) {
+    var F = function() {};
+    F.prototype = parent.prototype;
+    child.prototype = new F();
+    child.prototype.constructor = child;
+}
+inheritPrototype(SuperClass, f);
+function f() {
+    var res = [], r = [];
+    for (var i in this) {
+        r.push(i);
+        this.hasOwnProperty(i) ? res.push(i) : null;
     }
-    function sum(s1, s2){
-        for (i = 0; i < s1.length; i++)
-            for (j = 0; j < s1[0].length; j++)
-                s1[i][j] += s2[i][j];
-        return s1;
-    }
+    this.a = 1;
+    return 1;
+}
+var a = new f();
+var b = f();
 
-    function print(d) {
-        for (i = 0; i < d.length; i++) {
-                console.log(d[i].join(' '));
-            console.log('\n');
-        }
-    }
-    var set = [s];
-    var num = 5;
-    for(var i = 1; i <= num; ++i) {
-        set.push(c(_.cloneDeep(s), i));
-        console.log(i);
-
-    }
-    var res = set.slice(1).reduce(function(prev, item) {
-        return sum(prev, item);
-    }, set[0])
-    print(res);
-})();
