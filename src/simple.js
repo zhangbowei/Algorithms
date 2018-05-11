b72b4d924f7d8c411051a9491ae44703c539b6d8
ZhangBowei <919270881@qq.com>
2018-05-11 15:11:53 +0800

    Fri May 11 2018 15:11:53 GMT+0800 (CST)

--- src/simple.js
+++ src/simple.js
@@ -1,26 +1,15 @@
-function SuperClass(name) {
-    this.name = name;
-    this.books = ['JS', 'html', 'css'];
-}
-SuperClass.prototype.getName = function() {
-    console.log(this.name);
-}
-var inheritPrototype = function(parent, child) {
-    var F = function() {};
-    F.prototype = parent.prototype;
-    child.prototype = new F();
-    child.prototype.constructor = child;
-}
-inheritPrototype(SuperClass, f);
-function f() {
-    var res = [], r = [];
-    for (var i in this) {
-        r.push(i);
-        this.hasOwnProperty(i) ? res.push(i) : null;
-    }
-    this.a = 1;
-    return 1;
-}
-var a = new f();
-var b = f();
+var a = `{
+    'success': true,
+    'code': '00',
+    'm3g': '请求成功',
+    'data': [{
+        'user_no': '100033',
+        'username': 'hhoo',
+        'headpic_url': 'http://005.oo.cn/201853351322521.jpg',
+        'new_mag': '你好'
+    }]
+}`
+a = a.replace(/'/g, '"');
+var c = JSON.parse(a);
 
+console.log(c.data[0].username);
